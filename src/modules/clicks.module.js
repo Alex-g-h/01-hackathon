bb7995a4e1520b8e63b14f93b269edb1d3e64965
Ilya <ilyazhykevich@gmail.com>
2022-08-27 21:19:25 +0700

    click-module-develop

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,31 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
+  constructor() {
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    const clickTime = getRandomIntInclusive(5000, 10000);
+    let clickCount = 0;
 
+    alert(
+      `После нажатия на ОК будет подсчет кликов в любом месте экрана за время ${Math.round(
+        clickTime / 1000
+      )} секунд`
+    );
+    setTimeout(() => {
+      alert(`Количество кликов равно ${clickCount}`);
+      clickCount = 0;
+      document.removeEventListener("click", countClicks);
+    }, clickTime);
+
+    document.addEventListener("click", countClicks);
+
+    function countClicks() {
+      clickCount++;
+      console.log("click");
+    }
+  }
 }
