4fee0eec7415e27fda18f6a7c3cf8e90a0fcc537
Ruslan Gumirov <email@example.com>
2022-08-28 19:51:04 +0500

    пробел между классом и импорта

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,7 @@
 import {Module} from '../core/module'
+
 import {getRandomColor} from '../utils'
+
 export class BackgroundModule extends Module {
   trigger() {
     document.body.style.background = getRandomColor()
