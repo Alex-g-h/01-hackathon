383d42fd3d91f9343885b8d264b86e72eb0a1231
Ilya <ilyazhykevich@gmail.com>
2022-08-28 21:39:22 +0700

    final-module-commit

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -10,11 +10,13 @@ export class ClicksModule extends Module {
   }
 
   #countClicks() {
-    this.#clickCount += 1;
+    this.#clickCount++;
   }
 
   trigger() {
     const clickTime = random(5000, 10000);
+
+    this.#clickCount = 0;
 
     alert(
       `После нажатия на ОК будет подсчет кликов в любом месте экрана за время ${Math.round(
@@ -22,12 +24,14 @@ export class ClicksModule extends Module {
       )} секунд`
     );
 
+    let addCallbackFunc = this.#countClicks.bind(this);
+
     setTimeout(() => {
       alert(`Количество кликов равно ${this.#clickCount}`);
+
+      document.removeEventListener("click", addCallbackFunc);
     }, clickTime);
 
-    document.addEventListener("click", () => {
-      this.#countClicks();
-    });
+    document.addEventListener("click", addCallbackFunc);
   }
 }
