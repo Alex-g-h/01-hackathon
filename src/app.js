abe30d8b48d0b83985d847d480fab9431b40a958
Ilya <ilyazhykevich@gmail.com>
2022-08-28 17:08:00 +0700

    add-clicks-module

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-import './styles.css'
-import { ContextMenu } from './menu.js';
-import { TestModule } from './modules/test.module.js';
+import "./styles.css";
+import { ContextMenu } from "./menu.js";
+import { TestModule } from "./modules/test.module.js";
+import { ClicksModule } from "./modules/clicks.module.js";
 
+const contextMenu = new ContextMenu(".menu");
 
-const contextMenu = new ContextMenu('.menu');
+const clickModule = new ClicksModule("click", "click module");
+const testModule1 = new TestModule("test", "test module 1");
+const testModule2 = new TestModule("test", "test module 2");
+const testModule3 = new TestModule("test", "test module with very big text");
+const testModule4 = new TestModule(
+  "test",
+  "test module with huge length text for extended test for big height"
+);
 
-const testModule1 = new TestModule('test', 'test module 1');
-const testModule2 = new TestModule('test', 'test module 2');
-const testModule3 = new TestModule('test', 'test module with very big text');
-const testModule4 = new TestModule('test', 'test module with huge length text for extended test for big height');
-
+contextMenu.add(clickModule);
 contextMenu.add(testModule1);
 contextMenu.add(testModule2);
 contextMenu.add(testModule3);
