dfe76b11d2061ff63dc0f30ecebeaea8a08512f7
Ruslan Gumirov <email@example.com>
2022-08-28 23:14:17 +0500

    backgroundModule

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ const contextMenu = new ContextMenu('.menu');
 
 const testModule1 = new TestModule('test', 'test module 1');
 const testModule2 = new TestModule('test', 'test module 2');
-const testModule3 = new BackgroundModule();
+const backgroundModule = new BackgroundModule();
 const testModule4 = new TestModule('test', 'test module with huge length text for extended test for big height');
 
 contextMenu.add(testModule1);
 contextMenu.add(testModule2);
-contextMenu.add(testModule3);
+contextMenu.add(backgroundModule);
 contextMenu.add(testModule4);
